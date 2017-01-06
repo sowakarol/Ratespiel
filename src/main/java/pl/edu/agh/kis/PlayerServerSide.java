eacf3b8606924740ac8c4fd16fbc080fa75c075c
Karl <karosowa@student.agh.edu.pl>
2017-01-07 00:06:04 +0100

    Some small changes

--- src/main/java/pl/edu/agh/kis/PlayerServerSide.java
+++ src/main/java/pl/edu/agh/kis/PlayerServerSide.java
@@ -4,9 +4,9 @@ package pl.edu.agh.kis;
  * Created by Karl on 06.01.2017.
  */
 public interface PlayerServerSide {
+    boolean sendQuestion(QuestionServerSide);
 
     Answer answer(Reply reply);
-
 
     Reply getReply();
 }
