1fe18b5d27ffa7de6df9adeb98eb7e1153c4946b
Karl <karosowa@student.agh.edu.pl>
2017-01-07 00:06:56 +0100

    Some small changes v1.0

--- src/main/java/pl/edu/agh/kis/PlayerServerSide.java
+++ src/main/java/pl/edu/agh/kis/PlayerServerSide.java
@@ -4,7 +4,7 @@ package pl.edu.agh.kis;
  * Created by Karl on 06.01.2017.
  */
 public interface PlayerServerSide {
-    boolean sendQuestion(QuestionServerSide);
+    boolean sendQuestion(QuestionServerSide question);
 
     Answer answer(Reply reply);
 
