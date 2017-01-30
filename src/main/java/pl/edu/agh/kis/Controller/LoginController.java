c7746179f870f27e0402e5f8d522d6550ce40cbb
Karl <karosowa@student.agh.edu.pl>
2017-01-30 15:48:01 +0100

    Javadoc to LoginContoller

--- src/main/java/pl/edu/agh/kis/Controller/LoginController.java
+++ src/main/java/pl/edu/agh/kis/Controller/LoginController.java
@@ -21,82 +21,88 @@ public class LoginController implements ActionListener {
      * logging player mainFrame
      */
     private MainFrame mainFrame;
-
     /**
-     *
+     * loginPanel of a player who is logging
      */
     private LoginPanel loginPanel;
     /**
-     *
+     * true if everything was initialized corrected
      */
     private boolean initialized = false;
     /**
-     *
+     * server's hostname
      */
     private String hostname;
     /**
-     *
+     * username of player
      */
     private String username;
     /**
-     *
+     * portNumber of server
      */
     private int portNumber;
 
-
+    /**
+     * @param mainFrame mainFrame of logging player
+     */
     public LoginController(MainFrame mainFrame) {
         loginPanel = new LoginPanel(this);
         this.mainFrame = mainFrame;
     }
 
 
-
+    /**
+     * getter of LoginPanel
+     *
+     * @return reference to LoginPanel
+     */
     public LoginPanel getLoginPanel() {
         return loginPanel;
     }
 
 
-    public void setClicked(boolean clicked) {
-        this.clicked = clicked;
-    }
-
+    /**
+     * getter of initialized
+     * @return value of initialized variable
+     */
     public synchronized boolean isInitialized() {
         return initialized;
     }
 
-    public void setInitialized(boolean initialized) {
-        this.initialized = initialized;
-    }
-
+    /**
+     * getter of hostname
+     * @return hostname string value
+     */
     public String getHostname() {
         return hostname;
     }
 
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
 
+    /**
+     * getter of hostname variable
+     * for future functionalities
+     * @return username value
+     */
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
+    /**
+     * getter of portNumber value
+     * @return portNumber value
+     */
     public int getPortNumber() {
         return portNumber;
     }
 
-    public void setPortNumber(int portNumber) {
-        this.portNumber = portNumber;
-    }
-
+    /**
+     * taking care of a logging session - gets input data, checking if it's correct
+     * @param e buttons connected to this action
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("XD");
         if (!clicked) {
-            System.out.println("XXD");
             username = loginPanel.getUserText().getText();
             System.out.println(username);
             String tmp = loginPanel.getPortText().getText();
@@ -104,6 +110,7 @@ public class LoginController implements ActionListener {
             try {
                 portNumber = Integer.parseInt(tmp);
             } catch (NumberFormatException e1) {
+                //when cannot parseInt
                 e1.printStackTrace();
                 Dimension d = mainFrame.getSize();
                 WrongPortNumberFormatPanel wrongPortNumberFormatPanel = new WrongPortNumberFormatPanel();
@@ -116,12 +123,9 @@ public class LoginController implements ActionListener {
                 mainFrame.setVisible(true);
                 return;
             }
-            System.out.println(portNumber);
             hostname = loginPanel.getHostText().getText();
 
-
             initialized = true;
-            System.out.println(": (");
             clicked = true;
         }
 
